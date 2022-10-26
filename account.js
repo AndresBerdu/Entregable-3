a21aa3ea81a3a49b0b2414e6da3a5085bb956c4b
AndresBerdu <andres700crac@gmail.com>
2022-10-25 20:50:59 -0500

    Metodos Agregados
    Se agregaron los metodos a la API

--- account.js
+++ account.js
@@ -9,49 +9,45 @@ accountRouter.use((req, res, next) => {
   
     next()
 })
-//Obtener los detalles de una cuenta a partir del guid
-accountRouter.get('/:guid', (req, res) => {
-    const { guid } = req.params;
 
-    const user = USERS_BBDD.find( user => user.guid === guid)
-    
-    if (!user) return res.status(404).send()
-
-    return res.send(user)
-})
-
-//Crear una nueva cuenta
+//Crear un usuario 
 accountRouter.post('', (req, res) => {
 
-    const { name, guid } = req.body
+    const { _id, index ,guid, age, name, gender, company, email, phone } = req.body
 
     if (!name || !guid) return res.status(400).send();
-
 
     const user = USERS_BBDD.find( user => user.guid === guid)
     
     if (user) return res.status(409).send()
 
     USERS_BBDD.push({
-        name, guid
+        _id, index ,guid, age, name, gender, company, email, phone
     })
 
     return res.send()
 })
 
-//Actualizar el nombre de una cuenta
+//Actualizar un usuario
 accountRouter.put('/:guid', (req, res) => {
+   
     const { guid } = req.params
 
-    const { name } = req.body
+    const {index , age, name, gender, company, email, phone } = req.body
 
-    if (!name) return res.status(400).send();
+    if ( !name ) return res.status(400).send();
 
     const user = USERS_BBDD.find( user => user.guid === guid)
     
     if (!user) return res.status(404).send()
 
+    user.index = index
+    user.age = age
     user.name = name
+    user.gender = gender
+    user.company = company
+    user.email = email
+    user.phone = phone
 
     return res.send(user)
 })
@@ -67,6 +63,43 @@ accountRouter.delete('/:guid', (req, res) => {
     USERS_BBDD.splice(userIndex, 1)
 
     return res.send()
+})
+
+//Mostrar todos los usuarios 
+accountRouter.get('/users', (req, res) => {
+    return res.send(USERS_BBDD)
+})
+
+//Obtener los detalles de una cuenta a partir del guid
+accountRouter.get('/:guid', (req, res) => {
+    const { guid } = req.params;
+
+    const user = USERS_BBDD.find( user => user.guid === guid)
+    
+    if (!user) return res.status(404).send()
+
+    return res.send(user)
+})
+
+//Mostrar por compañia
+accountRouter.get('/company/:company', (req, res) => {
+    const { company } = req.params;
+
+    const user = USERS_BBDD.filter (user => user.company === company)
+
+    if(!user) return res.status(404).send()
+
+    return res.send(user)
+})
+
+//bustar todos los usuarios menores de 30 años
+accountRouter.get('/age/30', (req, res) => {
+
+    const user = USERS_BBDD.filter (user => user.age < 30)
+
+    if(user >= 30) return res.status(400).send
+
+    return res.send(user)
 })
 
 export default accountRouter
